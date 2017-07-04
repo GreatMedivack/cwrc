8ff987dbaff66131666b63f636d2c214660232a6
Medods <softs@cloud.medods.ru>
2017-07-04 13:31:09 -0400

    icons

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require 'telegram/bot'
 require 'date'
 require 'ap'
-token = '137469619:AAF6IlU6hHJEXNy6IcJDtK9lEqy1AwXDkrM'
+token = ''
 
 require 'sqlite3'
 
@@ -25,6 +25,7 @@ STOCK_LIFE_TIME = 60
 
 VALUABLE_ITEM = "\u{2B50}"
 SIMPLE_ITEM = "\u{1F539}"
+NOT_RESOURCE = "\u{1F458}"
 
 # Превый запуск
 
@@ -133,7 +134,7 @@ def update_profile(msg, user)
 end
 
 def share_stock
-	@db.execute "select item_types.title, sum(amount), item_types.valuable from items inner join item_types on items.item_type_id = item_types.cw_id where amount > 0 group by item_type_id order by item_types.valuable desc"
+	@db.execute "select item_types.cw_id, item_types.title, sum(amount), item_types.valuable from items inner join item_types on items.item_type_id = item_types.cw_id where amount > 0 group by item_type_id order by item_types.valuable desc"
 end
 
 kb =  [['Информация', 'Склад'], ['Спрятать ресурсы', 'Общий склад']]
@@ -145,7 +146,9 @@ VALID_IDS = [ 	259969632,  # Гномик
    	      			98141300,   # Админ
    	      			298568062,  # Кузя
 								387881985, 	# Димас
-				318388551 # Толстян
+				318388551, # Толстян
+				352073877, # Курва
+				435159344 # Толстян спойлер
 	    			]
 
 Telegram::Bot::Client.run(token) do |bot|
@@ -226,7 +229,8 @@ Telegram::Bot::Client.run(token) do |bot|
 
 			 	if message.text == 'Общий склад'
 			 		items = share_stock
-			 		stock = items.map {|item|  "\t\t\t\t #{item[2] == 1 ? VALUABLE_ITEM : SIMPLE_ITEM}_#{item[0]}_  *x#{item[1]}*" }
+					puts items
+			 		stock = items.map {|item|  "\t\t\t\t #{item[3] == 1 ? VALUABLE_ITEM : (item[0] / 1000 > 0) ? NOT_RESOURCE : SIMPLE_ITEM}_#{item[1]}_  *x#{item[2]}*" }
 			    	msg = stock.join("\n")
 			    	msg = "Пусто" if msg.empty?
 			    	bot.api.send_message(chat_id: message.from.id, parse_mode: 'Markdown', text: "\u{1F4DC}*Содержимое всех складов*\n#{msg}")
